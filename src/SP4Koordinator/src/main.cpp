a34aba4e85fd7192c6215e1930e387ece065a37d
Hubert Welp <hubert.welp@thga.de>
2025-05-25 17:01:00 +0200

    some further test with main.cpp

--- src/SP4Koordinator/src/main.cpp
+++ src/SP4Koordinator/src/main.cpp
@@ -51,7 +51,7 @@ int main(int argc, char **argv) {
   if (ros_enabled) {
       ros::init(argc, argv, "SP4Koordinator");
   }
-  printf("hello SP4");
+  printf("hello SP4Koordinator");
   // Create the main components
   Koordinator dialog(argc, argv);
   pds::MainWindow* window = new pds::MainWindow(&dialog);
